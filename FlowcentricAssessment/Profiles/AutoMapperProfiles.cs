b7050f1e74f6d84ad54d7a5a388c23851c86740e
christowessels <christowessels@gmail.com>
2024-02-11 08:37:30 +0200

    initial solution and project commit

---
+++ FlowcentricAssessment/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace FlowcentricAssessment.Profiles
+{
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
+    {
+      // Source -> Target
+      //CreateMap<Request_Register_DTO, IdentityUser>()
+      //          .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+      //          .ForMember(dest => dest.NormalizedEmail, opt => opt.MapFrom(src => src.Email.ToUpper()));
+
+    }
+  }
+}
